cd28a05628b50f38a6c1b3424b746dd84d0a5741
liang <445143561@qq.com>
2022-12-09 16:09:16 +0800

    非父子组件通信-消息订阅与发布

---
+++ src/Demos/非父子通信之消息订阅与发布.js
@@ -0,0 +1,114 @@
+import React, { Component } from "react";
+import axios from "axios";
+import PubSub from 'pubsub-js'
+
+
+class Son extends Component {
+
+
+  //有数据的地方，发布消息
+  isMovieDetails(synopsisName) {
+    PubSub.publish('data', synopsisName)
+
+  }
+  render() {
+    const { grade, name, poster, synopsis } = this.props;
+
+    return (
+      <div
+        onClick={() => { this.isMovieDetails(synopsis) }}
+
+        style={{
+          display: "flex",
+          alignItems: "center",
+          borderBottom: "1px solid gray",
+          margin: "0 10px 0",
+          padding: "10px",
+        }}
+      >
+        <img
+          src={poster}
+          style={{ height: "80px", width: "80px", paddingRight: "10px" }}
+          alt={name}
+        />
+        <div>
+          <h3 style={{ textAlign: "left" }}>{name}</h3>
+          <h3 style={{ textAlign: "left" }}>观众评分{grade}</h3>
+        </div>
+      </div>
+    );
+  }
+}
+
+class SonInfo extends Component {
+  state = {
+    name: ""
+  }
+  constructor() {
+    super()
+
+    //需要数据的地方，订阅消息
+    PubSub.subscribe('data', (_, synopsisName) => {
+      this.setState({
+        name: synopsisName
+      })
+    })
+  }
+
+  render() {
+    const { name } = this.state;
+
+    return (
+      <div
+        style={{
+          marginTop: "20px",
+          height: "250px",
+          background: "#EE82EE",
+          color: "#8A2BE2",
+        }}
+      >
+        <h2>我电影详情组件，电影信息为：</h2>
+        <h3>{name}</h3>
+      </div>
+    );
+  }
+}
+
+export default class Father extends Component {
+  constructor() {
+    super();
+    this.state = {
+      MovieList: [],
+      name: "",
+    };
+
+    axios({
+      url: "/test.json",
+      methods: "get",
+    })
+      .then((response) => {
+        // 因为层级比较深，匿名函数会导致this指向发生改变
+        // 这个时候使用箭头函数解决
+        console.log("数据是：", response.data.data.films);
+        this.setState({
+          MovieList: response.data.data.films,
+        });
+      })
+      .catch((err) => alert("网络超时, 请重新加载!"));
+  }
+
+  render() {
+    const { MovieList } = this.state;
+
+    console.log("接受数据是：", MovieList);
+
+    return (
+      <div>
+        {MovieList.map((i) => (
+          <Son {...i} key={i.filmId} />
+        ))}
+        <SonInfo />
+      </div>
+    );
+  }
+}
